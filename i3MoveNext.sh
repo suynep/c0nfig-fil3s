b675031a4389c1576742442e9b1fc87748f786a1
suy <suy.nepal@gmail.com>
2024-09-27 21:55:03 +0545

    modify i3 move scripts

--- i3MoveNext.sh
+++ i3MoveNext.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 wsNext=$(( $( i3-msg -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
-i3-msg workspace $wsNext
-i3-msg workspace $wsNext
+if [ $wsNext -ge 1 ]; then 
+  i3-msg workspace $wsNext
+else
+  i3-msg workspace number "4"
+fi
+
+if [ $wsNext -le 4 ]; then 
+  i3-msg workspace $wsNext
+else
+  i3-msg workspace number "1"
+fi
+
+if [ $wsNext == 0 ]; then
+  i3-msg workspace number "4"
+fi
